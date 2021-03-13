b88b66a8ef04513c972eecad20ff69bda3a25d62
Gaunk <ifdarus@yahoo.com>
2021-03-14 00:42:25 +0700

    Menambahkan TinyMCE

--- myblog/admin.py
+++ myblog/admin.py
@@ -2,9 +2,20 @@ from django.contrib import admin
 from .models import Post, Author, Kategori, ListHarga, PilihanHarga, TempatKursus
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Author)
 admin.site.register(Kategori)
 admin.site.register(ListHarga)
 admin.site.register(PilihanHarga)
 admin.site.register(TempatKursus)
+
+
+class PostAdmin(admin.ModelAdmin):
+    class Media:
+        css = {
+            "all": ("assets/css/main.css",)
+        }
+
+        js = ("assets/js/post.js",)
+
+
+admin.site.register(Post, PostAdmin)
---
+++ myblog/static/assets/js/post.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    let sc = document.createElement('script')
+    sc.setAttribute('src', 'https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js');
+
+    document.head.appendChild(sc);
+    sc.onload = () => {
+        tinymce.init({
+            selector: '#id_content'
+        });
+
+    }
+
+
+});
